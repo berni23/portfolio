f17e3c0676c2de31772dc29f30bd3b4a8a50a512
bernat-ferrer <bernatferrerm@gmail.com>
2021-01-24 23:49:05 +0100

    home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Title from "./name";
+import FadeIn from "react-fade-in";
+import Particles from "react-particles-js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title></Title>
+
+      <div className="stars"></div>
+
+      <div className="twinkling"></div>
+    </>
   );
 }
 
---
+++ src/name.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Title = () => {
+  const name = "BERNAT FERRER".split("");
+
+  React.useEffect(() => {
+    var nameContainer = document.querySelector(".name");
+
+    var lettersInt = setInterval(() => {
+      var letter = document.createElement("span");
+
+      letter.classList.add("fade-in");
+      letter.textContent = name.shift();
+      nameContainer.append(letter);
+
+      if (!name.length) clearInterval(lettersInt);
+    }, 500);
+  });
+
+  return (
+    <div className=" name-wrapper">
+      <h1 className="name"></h1>
+    </div>
+  );
+};
+
+export default Title;

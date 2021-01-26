950ce832eae0eb3eb8896e27fa90f24a79cdf9c2
bernat-ferrer <bernatferrerm@gmail.com>
2021-01-26 02:12:18 +0100

    lorem picsum

--- src/data.js
+++ src/data.js
@@ -41,6 +41,22 @@ const data = [
       text: "lorem picsum",
     },
   },
+
+  {
+    text: " Physics degree at UAB",
+    date: "September 15 2012",
+
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+
+    link: {
+      url: "https://picsum.photos/200",
+      text:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, autem reiciendis? Minus accusantium esse, quisquam ex perspiciatis veniam inventore similique exercitationem iste cupiditate voluptas voluptates magni quas, consectetur vitae odio quae hic possimus sed debitis molestiae tenetur? Saepe, labore id autem earum consequatur quos culpa quaerat quae necessitatibus! Quibusdam, nostrum?",
+    },
+  },
 ];
 
 export default data;
